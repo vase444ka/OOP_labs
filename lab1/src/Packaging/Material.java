c1fb61312aa94fa645b86c1e464a7e1f732e18c4
vase444ka <vborysyuk@live.com>
2020-12-25 02:06:35 +0200

    gift and packaging classes implemented

---
+++ lab1/src/Packaging/Material.java
@@ -0,0 +1,17 @@
+package Packaging;
+
+public enum Material {
+    PAPER(2),
+    PLASTIC(1),
+    WOOD(3),
+    POLYMER(1),
+    FOIL(2),
+    NONE(0);
+    private int _cost_per_gram;//cents
+
+    Material(int cost) {
+        _cost_per_gram = cost;
+    }
+
+    int getCostPerGram(){return _cost_per_gram;}
+}
